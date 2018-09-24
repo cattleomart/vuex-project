6addd60d7f554e420f70bc93a25d8f3bb4ef6ec7
Cathal O'Brien <Cathal@soops.intern>
2018-09-24 17:14:39 +0200

    end of tut

--- src/store.ts
+++ src/store.ts
@@ -3,8 +3,40 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+export const store = new Vuex.Store({
+  state: {
+    products: [
+      { name: "Banana Skin", price: 20 },
+      { name: "Shiny Star", price: 40 },
+      { name: "Green Shells", price: 60 },
+      { name: "Red Shells", price: 80 }
+    ]
+  },
+
+  actions: {
+    reducePrice: (context, payload ) => {
+      setTimeout(function() {
+        context.commit('reducePrice', payload);
+      }, 2000);
+    }
+  },
+  getters: {
+    saleProducts: state => {
+      var saleProducts = state.products.map(product => {
+        return {
+          name: "**" + product.name + "**",
+          price: product.price / 2
+        };
+      });
+
+      return saleProducts;
+    }
+  },
+  mutations: {
+    reducePrice: (state, payload) => {
+      state.products.forEach(product => {
+        product.price -= payload;
+      });
+    }
+  }
 });
